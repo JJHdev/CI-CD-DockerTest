e0c4c2698796744b88d0221a7e0740a614309e12
JJH <goodjob321@hanmail.net>
2024-04-13 19:45:38 +0900

    dockerfile 생성 후 commit

--- src/main/java/hyoun/mysite/MysiteApplication.java
+++ src/main/java/hyoun/mysite/MysiteApplication.java
@@ -6,7 +6,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class MysiteApplication {
 
-	public static void main(String[] args) {SpringApplication.run(MysiteApplication.class, args);
-	}
+	public static void main(String[] args) {SpringApplication.run(MysiteApplication.class, args);}
 
 }
