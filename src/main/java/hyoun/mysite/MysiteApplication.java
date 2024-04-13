0c4cc43796c582a8f993348ac7c650f3dacdb9ba
JJH <goodjob321@hanmail.net>
2024-04-13 17:56:14 +0900

    yml파일 변경

--- src/main/java/hyoun/mysite/MysiteApplication.java
+++ src/main/java/hyoun/mysite/MysiteApplication.java
@@ -7,7 +7,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class MysiteApplication {
 
 	public static void main(String[] args) {
-
 		SpringApplication.run(MysiteApplication.class, args);
 	}
 
